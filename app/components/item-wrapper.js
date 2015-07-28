1a84c90e39f859fc0ae61fbf4dfe46fa5314dd9f
Stefan Penner <stefan.penner@gmail.com>
2015-07-28 09:50:00 -0700

    remove the need for a route (and the outer attrs)

--- app/components/item-wrapper.js
+++ app/components/item-wrapper.js
@@ -1,9 +1,23 @@
 import Ember from 'ember';
 
+var model = Ember.Object.extend({
+  speed: 0,
+  moving: Ember.computed('speed', function() {
+    return this.get('speed') > 0;
+  })
+});
+
 export default Ember.Component.extend({
+  init() {
+    this._super(...arguments);
+    this.model = [model.create()];
+  },
+
   finished: true,
-  firstObject: Ember.computed.readOnly('items.firstObject'),
-  items: Ember.computed('showMoving', 'finished', function() {
+  firstObject: Ember.computed('items.[]', function() {
+    return this.get('items')[0];
+  }),
+  items: Ember.computed('finished', function() {
     if (this.get('finished')) {
       return [];
     }
